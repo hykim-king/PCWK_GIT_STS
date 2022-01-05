2defb814d88decb704dd7da70e2014bdf27d7cd7
hykim <hykim22@gmail.com>
2022-01-05 15:33:12 +0900

    최초작업

---
+++ src/main/java/com/pcwk/ehr/ed01/Hello99.java
@@ -0,0 +1,16 @@
+package com.pcwk.ehr.ed01;
+
+public class Hello99 {
+
+	public Hello99() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("===================");
+		System.out.println("Hello, world.");
+		System.out.println("===================");
+
+	}
+
+}
