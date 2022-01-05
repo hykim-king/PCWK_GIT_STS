613ba3e1d0052c5429336fd12c0ea96714e19a51
hykim <hykim22@gmail.com>
2022-01-05 17:22:09 +0900

    hostfix

--- src/main/java/com/pcwk/ehr/ed02/Hello.java
+++ src/main/java/com/pcwk/ehr/ed02/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("=================");
 		System.out.println("Hello, world!");
+		System.out.println("Hello, world!");
 		System.out.println("=================");
 
 	}
