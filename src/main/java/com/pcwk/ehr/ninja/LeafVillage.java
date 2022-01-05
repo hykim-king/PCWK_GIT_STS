bdc94854217d322ac17825e81ddfdfd83a32892e
jax <aprk456@gmail.com>
2022-01-05 16:10:56 +0900

    naruto

---
+++ src/main/java/com/pcwk/ehr/ninja/LeafVillage.java
@@ -0,0 +1,10 @@
+package com.pcwk.ehr.ninja;
+
+public class LeafVillage {
+
+	public static void main(String[] args) {
+		System.out.println("나루도~~~~~~");
+		System.out.println("쓰아쓰꼐~~~~~~");
+	}
+
+}
