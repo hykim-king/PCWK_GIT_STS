69158cd2d55650485afc8d1728854e25b9fc462a
hykim <hykim22@gmail.com>
2022-01-05 17:36:08 +0900

    branch

--- src/main/java/com/pcwk/ehr/ed02/Hello.java
+++ src/main/java/com/pcwk/ehr/ed02/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
 		System.out.println("=================");
 		System.out.println("Hello, world!");
 		System.out.println("Hello, world!");
+		System.out.println("Hello, world!");
 		System.out.println("=================");
 
 	}
